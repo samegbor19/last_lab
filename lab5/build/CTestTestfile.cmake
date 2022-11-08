3e4c882337dd9b10a36f7216d29e395745027909
Class Account <ee106a-adf@c105-9.local>
2022-11-08 14:56:11 -0800

    All odd labs

---
+++ lab5/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/lab5/src
+# Build directory: /home/cc/ee106a/fa22/class/ee106a-adf/ros_workspaces/lab5/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ik")
+subdirs("move_arm")
