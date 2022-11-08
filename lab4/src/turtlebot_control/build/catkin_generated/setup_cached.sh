0b5485804d830a3ad5f324feb1062a7503c1f357
Class Account <ee106a-aht@c105-12.local>
2022-11-08 15:57:14 -0800

    all even labs

---
+++ lab4/src/turtlebot_control/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cc/ee106a/fa22/class/ee106a-aht/ros_workspaces/lab4/src/turtlebot_control/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/cc/ee106a/fa22/class/ee106a-aht/ros_workspaces/lab4/src/turtlebot_control/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cc/ee106a/fa22/class/ee106a-aht/ros_workspaces/lab4/src/turtlebot_control/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/cc/ee106a/fa22/class/ee106a-aht/ros_workspaces/lab4/src/turtlebot_control/src:$ROS_PACKAGE_PATH"
